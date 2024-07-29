e921b70412bd5bc99e8f9c631435eb8f0098a3b6
moosedev <ahmed.musawir@gmail.com>
2024-07-29 10:21:09 +0800

    29July2024 - done w/ a few bugs

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,47 +1,22 @@
 "use client";
 
-import { ReactNode, useEffect } from "react";
-import { useAuthStore } from "@/store/useAuthStore";
-import Spinner from "@/components/common/Spinner";
-import { useRouter } from "next/navigation";
+import { ReactNode } from "react";
 import Navbar from "@/components/global/Navbar";
+import withAdminProtection from "@/hoc/withAdminProtection";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const AdminLayout = ({ children }: LayoutProps) => {
-  const router = useRouter();
-  const roles = useAuthStore((state) => state.roles);
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      router.push("/auth");
-    } else if (roles.is_qr_admin !== 1) {
-      router.push("/auth");
-    }
-  }, [isAuthenticated, roles, router]);
-
-  if (!isAuthenticated || roles.is_qr_admin !== 1) {
-    return (
-      <div>
-        <Spinner />
-      </div>
-    );
-  }
-
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="flex flex-1">
-        {/* <div className="hidden md:block h-auto flex-shrink-0 border-4 w-[25rem]">
-          <Sidebar />
-        </div> */}
         <div className="flex-1 p-5 md:max-w-[1140px]">{children}</div>
       </div>
     </div>
   );
 };
 
-export default AdminLayout;
+export default withAdminProtection(AdminLayout);
